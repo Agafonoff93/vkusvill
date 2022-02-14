f7a269032e6617e1a807c8d65891044804d4e1c9
Evgeniy <megafon-cool@mail.ru>
2022-02-14 20:16:38 +0300

    maybe i all so

--- ametion_vv-test/js/app.js
+++ ametion_vv-test/js/app.js
@@ -876,46 +876,7 @@ animate({
 			Element.prototype.msMatchesSelector;
 	}
 })();
+alert('привет я работаю')
 
 
 
-$(document).ready(function(){
-	$('.slider-faq__body').slick({
-		dots: false,
-		infinite: false,
-		speed: 300,
-		slidesToShow: 4,
-		slidesToScroll: 1,
-		variableWidth: true,
-		responsive: [
-		  {
-			 breakpoint: 1024,
-			 settings: {
-				slidesToShow: 3,
-				slidesToScroll: 3,
-				
-				
-			 }
-		  },
-		  {
-			 breakpoint: 600,
-			 settings: {
-				arrows: false,
-				slidesToShow: 2,
-				slidesToScroll: 2
-			 }
-		  },
-		  {
-			 breakpoint: 480,
-			 settings: {
-				arrows: false,
-				slidesToShow: 1,
-				slidesToScroll: 1
-			 }
-		  }
-		  // You can unslick at a given breakpoint now by adding:
-		  // settings: "unslick"
-		  // instead of a settings object
-		]
-	 });
-});
